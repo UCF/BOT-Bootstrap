c72c663cfdb71234413eb2d9a11faba613c92059
Jo Greybill <jogreybill@gmail.com>
2013-04-17 11:19:24 -0400

    Display 'no biography available' message if no post content for Person is available

--- single-person.php
+++ single-person.php
@@ -1,4 +1,4 @@
-<?php disallow_direct_load('single.php');?>
+<?php disallow_direct_load('single-person.php');?>
 <?php get_header(); the_post();?>
 	<div class="page-content person-profile">
 		<div class="row">
@@ -25,7 +25,8 @@
 			<div class="span6">
 				<h2><?=$post->post_title?></h2>
 				<?php if ($title) { ?><h3 class="page-subtitle"><?=$title?></h3><? } ?>
-				<?=$content = str_replace(']]>', ']]>', apply_filters('the_content', $post->post_content))?>
+				<?php if ($post->post_content) { echo the_content(); }
+					  else { ?><p><em>No biography available.</em></p><?php } ?>
 			</div>
 			<?=get_sidebar();?>
 		</div>
