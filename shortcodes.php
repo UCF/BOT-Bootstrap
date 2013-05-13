d6bb749454d7e8328cd13af982f198ce6368357b
Brandon Groves <brandon.groves@ucf.edu>
2013-05-13 09:39:01 -0400

    limited the committee meeting lists to the current year and next.

--- shortcodes.php
+++ shortcodes.php
@@ -172,15 +172,21 @@ function sc_committee_meetings($attrs = array()) {
 						<td colspan="4">No meetings scheduled.</td>
 					</tr>
 				<? else: ?>
-					<?php meetings_prep($meetings); foreach($meetings as $i=>$meeting):?>
-						<?php if (get_post_meta($meeting->ID, 'meeting_date', true)) { ?>
-							<tr class="<?=($i % 2) ? 'even' : 'odd';?>">
+					<?php meetings_prep($meetings); $row_cnt = -1; foreach($meetings as $i=>$meeting):?>
+						<?php
+							if (get_post_meta($meeting->ID, 'meeting_date', true)) {
+								$today = getdate();
+								$year  = (isset($_GET['y'])) ? $_GET['y'] : $today['year'];
+								$meeting_year = date("Y", strtotime(get_post_meta($meeting->ID, 'meeting_date', true)));
+								if ($meeting_year >= $year) {
+						?>
+							<tr class="<?=(++$row_cnt % 2) ? 'even' : 'odd';?>">
 								<td class="date"><?=date("F j, Y", strtotime(get_post_meta($meeting->ID, 'meeting_date', true)))?></td>
 								<td class="time">
 									<?php
 										$start = get_post_meta($meeting->ID, 'meeting_start_time', True);
 										$end   = get_post_meta($meeting->ID, 'meeting_end_time', True);
-										$special_title = get_post_meta($meeting->ID, 'meeting_special_meeting', True); 
+										$special_title = get_post_meta($meeting->ID, 'meeting_special_meeting', True);
 									?>
 									<?php if($start and $end):?>
 										<?=$start?> &ndash; <?=$end?>
@@ -221,8 +227,14 @@ function sc_committee_meetings($attrs = array()) {
 								</td>
 								<td class="location"><?=get_post_meta($meeting->ID, 'meeting_location', True)?></td>
 							</tr>
+							<?php } ?>
 						<?php } ?>
 					<?php endforeach;?>
+					<?php if ($row_cnt == -1): ?>
+						<tr>
+							<td colspan="4">No meetings scheduled.</td>
+						</tr>
+					<?php endif; ?>
 				<?php endif; ?>
 				</tbody>				
 			</table>
