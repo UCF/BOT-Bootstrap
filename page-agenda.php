0e45988c82bc39c8d29aea2afb797bb3ea72a70e
RJ Bruneel <ra368420@cmd25nm06nf8j9.net.ucf.edu>
2017-03-27 13:31:38 -0400

    removed old page templates

--- page-agenda.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); the_post();?>
-	<?php
-		$agendas = true;
-		$minutes = false;
-	?>
-	<div class="row page-content" id="<?=$post->post_name?>">
-		<?=get_sidebar();?>
-	</div>
-<?php get_footer(); ?>
--- page-minutes.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); the_post();?>
-	<?php
-		$agendas = false;
-		$minutes = true;
-	?>
-	<div class="row page-content" id="<?=$post->post_name?>">
-		<?=get_sidebar();?>
-	</div>
-<?php get_footer(); ?>
