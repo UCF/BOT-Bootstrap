afa06c534f38e3674faade89b15122117b395509
RJ Bruneel <ra368420@cmd25nm06nf8j9.net.ucf.edu>
2017-03-28 17:07:32 -0400

    Initial footer changes push

--- footer.php
+++ footer.php
@@ -1,37 +1,34 @@
-			<?=wp_nav_menu(array(
-				'theme_location' => 'footer-menu',
-				'container'      => 'false',
-				'menu_class'     => 'menu horizontal',
-				'menu_id'        => 'footer-menu',
-				'fallback_cb'    => false,
-				'depth'          => 1,
-				'walker'         => new Bootstrap_Walker_Nav_Menu()
-				));
-			?>
-			<div id="footer">
-				<div class="row">
-					<div class="span9">
-						<a class="ignore-external" href="http://www.ucf.edu"><img src="<?=THEME_IMG_URL?>/logo.png" alt="" title="" /></a>
-					</div>
-					<div class="span3">
-						<?php $options = get_option(THEME_OPTIONS_NAME);?>
-						<?php if($options['site_contact'] or $options['organization_name']):?>
-						<p>
-							Site maintained by the <br />
-							<?php if($options['site_contact'] and $options['organization_name']):?>
-							<a href="mailto:<?=$options['site_contact']?>"><?=$options['organization_name']?></a>
-							<?php elseif($options['site_contact']):?>
-							<a href="mailto:<?=$options['site_contact']?>"><?=$options['site_contact']?></a>
-							<?php elseif($options['organization_name']):?>
-							<?=$options['organization_name']?>
-							<?php endif;?>
-						</p>
-						<?php endif;?>
-						<p>&copy; University of Central Florida</p>
+				</div>
+			</div>
+		<footer>
+			<div class="footer-menu-wrapper">
+				<div class="container">
+					<div class="row footer-menu-wrapper">
+						<div class="col-md-8">
+						<?php
+							echo wp_nav_menu(array(
+								'theme_location' => 'header-menu',
+								'container'      => 'false',
+								'menu_class'     => 'menu list-unstyled list-inline',
+								'menu_id'        => 'footer-menu',
+								'walker'         => new Bootstrap_Walker_Nav_Menu()
+							));
+						?>
+						</div>
+						<div class="col-md-4">
+							[Search Goes Here]
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-align-center">
+						&copy; University of Central Florida
+					</div>
+				</div>
+			</div>
+		</footer>
 	</body>
 	<?="\n".footer_()."\n"?>
 </html>
