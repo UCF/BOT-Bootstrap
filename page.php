919528fc1941879c4666a9b7b63b531fd5a9e6c7
Jim Barnes <james.barnes@ucf.edu>
2017-03-30 16:45:20 -0400

    Updated layout on page template

--- page.php
+++ page.php
@@ -1,11 +1,11 @@
 <?php get_header(); the_post();?>
 <div class="container">
 	<div class="row page-content" id="<?=$post->post_name?>">
-		<div class="col-md-8">
+		<div class="col-md-9">
 			<h1 class="page-title"><?php the_title(); ?></h1>
 			<?php the_content();?>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
