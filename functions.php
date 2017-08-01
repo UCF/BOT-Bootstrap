6fb85478da0cbc509760430829a20a063f60d273
Keegan Berry <keeganberry@gmail.com>
2017-08-01 16:40:27 -0400

    fix meeting list on meetings page

--- functions.php
+++ functions.php
@@ -226,7 +226,6 @@ function display_meetings( $meetings ) {
 		$start = isset( $post->metadata['ucf_meeting_start_time'] ) ? $post->metadata['ucf_meeting_start_time'] : null;
 		$end = isset( $post->metadata['ucf_meeting_end_time'] ) ? $post->metadata['ucf_meeting_end_time'] : null;
 		$location = isset( $post->metadata['ucf_meeting_location'] ) ? $post->metadata['ucf_meeting_location'] : 'TBD';
-		$special_meeting_name = isset( $post->metadata['ucf_meeting_special_name'] ) ? $post->metadata['ucf_meeting_special_name'] : null;
 	?>
 			<tr>
 				<td><?php echo $date; ?></td>
@@ -240,9 +239,6 @@ function display_meetings( $meetings ) {
 				<?php endif; ?>
 				</td>
 				<td><?php echo $location; ?>
-					<?php if ( isset( $special_meeting_name ) ) : ?>
-						<br /><i><?php echo $special_meeting_name; ?></i>
-					<?php endif; ?>
 				</td>
 				<td class="text-center">
 					<?php if ( isset( $post->metadata['ucf_meeting_agenda'] ) && ! empty( $post->metadata['ucf_meeting_agenda'] ) ) : ?>
