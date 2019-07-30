66bab24ed089ccf0e7a8dcc65a0f2d8ddc14a960
RJ Bruneel <rj.bruneel@ucf.edu>
2019-07-30 10:04:43 -0400

    minor tweaks

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="form-search" action="https://search.ucf.edu">
 	<div>
 		<div class="form-group mb-0">
-			<input type="text" name="q" class="form-control" id="q" placeholder="Search..." />
+			<input type="text" name="q" class="form-control" id="q" placeholder="Search...">
 			<input type="hidden" name="sitesearch" value="bot.ucf.edu">
 			<input type="submit" class="d-none">
 		</div>
